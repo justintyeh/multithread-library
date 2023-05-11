bd46135c08fe844d8666a145d02657af0540efe5
jtyeh <jtyeh@COE-CS-pc21.OU.AD3.UCDAVIS.EDU>
2023-05-11 04:22:17 -0700

    start preempt

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -18,20 +18,32 @@
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
+  // disable preemption
 }
 
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
+  // enable preemption
 }
 
 void preempt_start(bool preempt)
 {
 	/* TODO Phase 4 */
+  if (preempt){
+    //start preemption
+    // configure a timer that must fire a virutal alarm w/ freq 100Hz
+    // set up timer handler that forcefully yields currently running thread
+  }
+  // if preempt false then all other function should be ineffective
 }
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+  // stop thread preemption
+
+  // restore previous timer configuration and prev action associated with virt.
+  // alarm sigs
 }
 
