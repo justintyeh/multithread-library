e62df24831088f9f290296f2d81f47905410faef
justintyeh <justintyeh@gmail.com>
2023-05-11 23:12:48 -0700

    update test preempt

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -15,36 +15,47 @@
 #include <stdlib.h>
 
 #include "uthread.h"
+int set;
+int set2;
 
 void thread3(void *arg)
 {
 	(void)arg;
-
-	uthread_yield();
 	printf("thread3\n");
+	set2 = 1;
 }
 
 void thread2(void *arg)
 {
 	(void)arg;
-
-	uthread_create(thread3, NULL);
-	uthread_yield();
+	// uthread_yield();
+	set2 = 0;
 	printf("thread2\n");
+	uthread_create(thread3, NULL);
+	while (1)
+	{
+		if (set2 == 1)
+		{
+			set = 1;
+			break;
+		}
+	}
+	printf("finished thread2\n");
 }
 
 void thread1(void *arg)
 {
 	(void)arg;
-
+	set = 0;
+	// uthread_yield();
+	printf("thread1\n");
 	uthread_create(thread2, NULL);
-	uthread_yield();
-		  printf("thread1\n");
-	while (1){
-	  ;
+	while (1)
+	{
+		if (set == 1)
+			break;
 	}
-	
-	uthread_yield();
+	printf("finished thread1\n");
 }
 
 int main(void)
@@ -52,3 +63,14 @@ int main(void)
 	uthread_run(true, thread1, NULL);
 	return 0;
 }
+/*
+Results should be...
+thread1
+-> enter while loop in thread 1
+thread2
+-> enter while loop in thread 2
+thread3
+-> finished thread 2
+-> finished thread 1
+
+*/
