25f9ae3a6d36cf1ef0a1fc23189ea2b73226b970
Justin Yeh <justintyeh@gmail.com>
2023-05-11 17:39:28 -0700

    cleaned up files

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <queue.h>
+#include "queue.h"
 
 #define TEST_ASSERT(assert)				\
 do {									\
@@ -35,12 +35,95 @@ void test_queue_simple(void)
 	queue_enqueue(q, &data);
 	queue_dequeue(q, (void**)&ptr);
 	TEST_ASSERT(ptr == &data);
+	int qlen = queue_length(q);
+	TEST_ASSERT(0 == qlen);
 }
+
+// eq and dq multiple
+void mtest(void)
+{
+  queue_t q;
+  fprintf(stderr, "*** TEST mtest ***\n");
+
+  q = queue_create();
+
+  int a = 1;
+  queue_enqueue(q, &a);
+  
+  int b = 2;
+  queue_enqueue(q, &b);
+  
+  int c = 3;
+  queue_enqueue(q, &c);
+  
+  int d = 4;
+  queue_enqueue(q, &d);
+
+  TEST_ASSERT(queue_length(q) == 4);
+
+  int *ptr;
+  queue_dequeue(q, (void**)&ptr);
+  TEST_ASSERT(ptr == &a);
+
+  queue_dequeue(q, (void**)&ptr);
+  TEST_ASSERT(ptr == &b);
+
+  TEST_ASSERT(queue_length(q) == 2);
+}
+
+void destroy(void){
+  queue_t q;
+  fprintf(stderr, "*** TEST destroy ***\n");
+  q = queue_create();
+  int x = queue_destroy(q);
+  TEST_ASSERT(0 == x);
+}
+
+void delete(void){
+  queue_t q;
+  fprintf(stderr, "*** TEST delete ***\n");
+  
+  q = queue_create();
+
+  int a = 1;
+  queue_enqueue(q, &a);
+  
+  int b = 2;
+  queue_enqueue(q, &b);
+  
+  int c = 3;
+  queue_enqueue(q, &c);
+  
+  int d = 4;
+  queue_enqueue(q, &d);
+
+  int x = queue_delete(q, &c);
+
+  TEST_ASSERT(0 == x);
+
+  TEST_ASSERT(-1 == queue_destroy(q));
+
+  int *p1, *p2, *p3;
+  queue_dequeue(q, (void**)&p1);
+  //  queue_dequeue(q,(void**)&p2);
+  // queue_dequeue(q,(void**)&p3);
+  TEST_ASSERT(0 == queue_length(q));
+}
+
+// enqueue NULL item and see if function returns proper error code
+// delete item that does not exist
+//
+
+/* void errors(void){ */
+  
+/* } */
 
 int main(void)
 {
 	test_create();
 	test_queue_simple();
-
+	mtest();
+	destroy();
+	delete();
 	return 0;
 }
--- apps/queue_tester_example.d
+++
@@ -1,1 +0,0 @@
-queue_tester_example: queue_tester_example.c ../libuthread/queue.h
